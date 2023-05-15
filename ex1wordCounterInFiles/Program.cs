de4b75757913533bc5b8d4d1f37648a58d83909d
Mateusz Olczyk <mat.olczyk@gmail.com>
2023-05-15 22:29:33 +0200

    hangman game with graphics

---
+++ ex1wordCounterInFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("elo");
+
+var folder = @".\Test";
+var txtFiles = Directory.GetFiles(folder, "*.txt");
+
+
+
+
+foreach (var txtFile in txtFiles)
+{
+    var readFile = File.ReadAllText(txtFile);
+    var str = readFile.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+    System.Console.WriteLine($"Number of words in {txtFile}: " + str.Length);
+}
+
+
+    Console.ReadLine();
---
+++ ex1wordcounter/TxtFileCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1wordcounter
+{
+    internal class TxtFileCalculator
+    {
+
+        public string[] GetFiles (string folder)
+        {
+            var txtFiles = Directory.GetFiles(folder, "*.txt");
+            return txtFiles;
+        }
+
+        public int GetWords (string txtFile) 
+        {
+            var readFile = File.ReadAllText(txtFile);
+            var str = readFile.Split(new char[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int counter = str.Length;
+            return counter;
+        }
+
+    }
+}
